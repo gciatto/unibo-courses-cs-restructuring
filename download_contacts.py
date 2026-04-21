e9c5f2ba99870562428302674834a93d107fcbab
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2026-04-21 09:03:40 +0200

    feat: initialize repo with data about contacts and courses, and placeholder scripts

---
+++ download_contacts.py
@@ -0,0 +1,10 @@
+import pathlib
+
+
+DATA_DIR = pathlib.Path("data")
+DEFAULT_OUTPUT = DATA_DIR / "contacts.csv"
+
+
+if __name__ == "__main__":
+    print(f"TODO: implement this script so that {DEFAULT_OUTPUT} is re-created")
+    exit(1)
